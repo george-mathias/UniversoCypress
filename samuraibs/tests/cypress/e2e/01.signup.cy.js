1c992ac554bb19b0dc214b45a59c67968ce99d4a
George Mathias <gmathias80@gmail.com>
2022-07-24 08:28:29 -0300

    class 02

--- samuraibs/tests/cypress/e2e/01.signup.cy.js
+++ samuraibs/tests/cypress/e2e/01.signup.cy.js
@@ -1,17 +1,4 @@
-
-// não faz parte do treinamento
-// const selectors = {
-//     input: {
-//         name: 'input[placeholder="Nome"]',
-//         email: 'input[placeholder="E-mail"]',
-//         pass: 'input[placeholder="Senha"]'
-//     },
-//     dados: {
-//         name: "George Mathias",
-//         email: "fulano@gmail.com",
-//         pass: "abc123"
-//     }
-// }
+import signUpPage from '../support/pages/signup'
 
 describe('cadastro', () => {
 
@@ -19,7 +6,7 @@ describe('cadastro', () => {
         const user = {
             name: "George Mathias",
             email: "a@gmail.com",
-            pass: "pwd123"
+            password: "pwd123"
         }
 
         before(() => {
@@ -37,31 +24,10 @@ describe('cadastro', () => {
             Então deve cadastrar o usuário
             E deve exibir uma mensagem de sucesso`, () => {
 
-            cy.visit("/")
-
-            cy.contains("a", "Criar conta")
-                .click()
-
-            cy.contains('h1', "Faça seu cadastro")
-                .should('have.text', 'Faça seu cadastro')
-
-            // não faz parte do treinamento
-            // Object.keys(selectors.input).forEach((selector) => {
-            //     cy.get(selectors.input[selector])
-            //         .should('be.visible')
-            //         .type(selectors.dados[selector])
-            // }) 
-
-            cy.get('input[placeholder^="Nome"]').type(user.name)
-            cy.get('input[placeholder$="email"]').type(user.email)
-            cy.get('input[placeholder*="senha"]').type(user.pass)
-
-            cy.contains("button", "Cadastrar").click()
-
-            cy.get('.toast')
-                .should('be.visible')
-                .find('p')
-                .should('have.text', 'Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
+            signUpPage.go()
+            signUpPage.form(user)
+            signUpPage.submit()
+            signUpPage.toastHaveText('Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
         })
     });
 
@@ -97,21 +63,10 @@ describe('cadastro', () => {
             Então não deve realizar o cadastrar
             E deve exibir uma mensagem email já cadastrado`, () => {
 
-            cy.visit("/signup")
-
-            cy.contains('h1', "Faça seu cadastro")
-                .should('have.text', 'Faça seu cadastro')
-
-                cy.get('input[placeholder^="Nome"]').type(user.name) // começa com
-                cy.get('input[placeholder$="email"]').type(user.email) // termina com
-                cy.get('input[placeholder*="senha"]').type(user.password) // contém
-
-            cy.contains("button", "Cadastrar").click()
-
-            cy.get('.toast')
-                .should('be.visible')
-                .find('p')
-                .should('have.text', 'Email já cadastrado para outro usuário.')
+                signUpPage.go()
+                signUpPage.form(user)
+                signUpPage.submit()
+                signUpPage.toastHaveText('Email já cadastrado para outro usuário.')
         })
     });
 });
