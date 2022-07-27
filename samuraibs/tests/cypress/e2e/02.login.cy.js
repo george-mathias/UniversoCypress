9f266d4a0bdec8acb8e58d380262d7fa9d76b3b7
George Mathias <gmathias80@gmail.com>
2022-07-27 08:47:50 -0300

    class 02

--- samuraibs/tests/cypress/e2e/02.login.cy.js
+++ samuraibs/tests/cypress/e2e/02.login.cy.js
@@ -1,21 +1,24 @@
 
-
 import loginPage from '../support/pages/login'
+import dashPage from '../support/pages/dash'
 
 describe('login', () => {
-    
+
     context('quando o usuário é muito bom', () => {
-        
+
         const user = {
+            name: 'Robson Jassa',
             email: 'jassa@samuraibs.com',
             password: 'pwd123'
         }
 
-        it('deve logar com sucesso', () => {
-            
+        it.only('deve logar com sucesso', () => {
+
             loginPage.go()
             loginPage.form(user)
             loginPage.submit()
+
+            dashPage.userLoggedIn(user.name)
         });
     });
 });
