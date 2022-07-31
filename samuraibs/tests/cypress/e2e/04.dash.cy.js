0b2a4da45e0c4a1f92cf8e98c1220cab7f722076
George Mathias <gmathias80@gmail.com>
2022-07-31 18:25:26 -0300

    class 06: refactorin mehtods setProviderId and apiLogin

--- samuraibs/tests/cypress/e2e/04.dash.cy.js
+++ samuraibs/tests/cypress/e2e/04.dash.cy.js
@@ -2,7 +2,7 @@ describe('dashboard', () => {
 
     context('quando o cliente faz um agendamento no app mobile', () => {
 
-        
+
         const data = {
             customer: {
                 name: "Nikki Sixx",
@@ -18,69 +18,59 @@ describe('dashboard', () => {
             }
         }
 
-        let providerId = null
         before(function () {
             cy.postUser(data.provider)
             cy.postUser(data.customer)
 
-            const payload = {
-                email: data.customer.email,
-                password: data.customer.password
-            }
-
-            cy.request({
-                method: "POST",
-                url: "http://localhost:3333/sessions",
-                body: payload
-            }).then((response) => {
-                expect(response.status).to.eq(200)
-                cy.log(response.body.token)
-
-            }).then((response) => {
-
-                cy.request({
-                    method: "GET",
-                    url: "http://localhost:3333/providers",
-                    headers: { authorization: `Bearer ${response.body.token}` }
-                }).then((response) => {
-
-                    const providerList = response.body
-                    
-                    providerList.forEach((provider) => {
-                        // cy.log('provider: ', provider.id)
-                        if (provider.email === data.provider.email) { 
-                            providerId = provider.id
-                        }
-                    })
-                    // cy.log('providerId: ', providerId)
-                })
-            })
-
-
+            cy.apiLogin(data.customer)
+            cy.setProviderId(data.provider.email)
+            
         });
+
         it('o mesmo deve ser exibido no dashboard', function () {
-            // cy.log('token', token)
-            // cy.setProviderId(token)
-            cy.log('providerId: ', providerId)
+            cy.log('providerId: ', Cypress.env('providerId'))
+
         });
 
     });
 
 });
 
-// Cypress.Commands.add('setProviderId', (token, providerEmail) => {
-//     cy.request({
-//         method: "GET",
-//         url: "http://localhost:3333/providers",
-//         headers: { authorization: `Bearer ${token}` }
-//     }).then((response) => {
+Cypress.Commands.add('apiLogin', (user) => {
 
-//         const providerList = response.body
-//         // cy.log('log', response.body[0])
-//         // cy.log(response.body[0].id)
-//         providerList.forEach((provider) => {
-//             cy.log(provider.id)
-//             if( providerList === providerEmail) {}
-//         })
-//     })
-// })
+    const payload = {
+        email: user.email,
+        password: user.password
+    }
+
+    cy.request({
+        method: "POST",
+        url: "http://localhost:3333/sessions",
+        body: payload
+    }).then((response) => {
+        expect(response.status).to.eq(200)
+        cy.log(response.body.token)
+        Cypress.env('apiToken', response.body.token)
+    })
+})
+
+Cypress.Commands.add('setProviderId', (providerEmail) => {
+    cy.request({
+        method: "GET",
+        url: "http://localhost:3333/providers",
+        headers: {
+            authorization: `Bearer ${Cypress.env('apiToken')}`
+        }
+    }).then((response) => {
+        cy.log(response.body)
+
+        const providerList = response.body
+
+        providerList.forEach((provider) => {
+            // cy.log('provider: ', provider.id)
+            if (provider.email === providerEmail) {
+                Cypress.env('providerId', provider.id)
+            }
+        })
+    })
+})
